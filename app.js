994ac97f7cb760dc2168ef933468357b904f1e22
ほと <mail@hoto.me>
2016-03-10 00:14:59 +0900

    nodemon

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.json({ error: 'Not Found' });
+  res.status(404).json({ error: 'Not Found' });
 });
 
 app.use((err, req, res, next) => {
