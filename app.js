1f13b26fbf9ced093a876f2aed3f2bf1847b8049
ほと <mail@hoto.me>
2016-03-09 23:51:55 +0900

    error handling

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  if ( res.headersSent ) { return next(err); }
+  res.status( err.status || 500 );
+  res.json({ error: err.message });
+});
+
 app.listen( process.env.PORT || 3000 );
