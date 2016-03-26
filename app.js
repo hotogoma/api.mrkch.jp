d1b4c6c29934df3024ae689054e4d2b9b2ca86ec
ほと <mail@hoto.me>
2016-03-26 12:11:41 +0900

    温度を測る

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 'use strict';
 
 var express = require('express');
+var five = require('johnny-five');
+
 var app = express();
+var board = new five.Board();
+
+var store = {};
+
+board.on('ready', () => {
+  var temperature = new five.Temperature({
+    controller: 'LM35',
+    pin: 'A0',
+    freq: 1000,
+  });
+  temperature.on('data', (data) => {
+    store.temperature = data;
+  });
+});
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+  res.json(store);
+});
+
+app.get('/metric', (req, res) => {
+  var sec = Math.floor( new Date().getTime() / 1000 );
+  var metric = {
+    temperature: Math.round( store.temperature.celsius * 10 ) / 10,
+  };
+  var arr = [];
+  for ( var key in metric ) {
+    arr.push( [ key, metric[key], sec ].join("\t") );
+  }
+  res.send( arr.join("\n") );
 });
 
 app.use((req, res, next) => {
